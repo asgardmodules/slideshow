4e03f1f24177dfde37321cea05f283621a495bf4
unknown <Michel>
2013-11-03 20:09:53 +1100

    libification

--- bundle.php
+++ bundle.php
@@ -1,16 +1,28 @@
 <?php
-Config::set('slideshow', 'width', 1000);
-Config::set('slideshow', 'height', 768);
+namespace App\Slideshow;
 
-\Coxis\App\Imagecache\Libs\ImageCache::addPreset('imagecache', array(
-	'resize'	=>	array(
-		'width'	 =>	Config::get('slideshow', 'width'),
-	),
-	'crop'	=>	array(
-		'height' =>	Config::get('slideshow', 'height'),
-	)
-));
+class Bundle extends \Coxis\Core\BundleLoader {
+	public function load($queue) {
+		parent::load();
+	}
 
-\Coxis\App\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Slideshow', 'link' => 'slideshow');
+	public function run() {
+		Config::set('slideshow', 'width', 1000);
+		Config::set('slideshow', 'height', 768);
 
-\Coxis\App\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('slideshow/icon.svg'), 'link'=>'slideshow', 'title' => __('Slideshow'), 'description' => __('Slideshow images'));
+		\App\Imagecache\Libs\ImageCache::addPreset('imagecache', array(
+			'resize'	=>	array(
+				'width'	 =>	Config::get('slideshow', 'width'),
+			),
+			'crop'	=>	array(
+				'height' =>	Config::get('slideshow', 'height'),
+			)
+		));
+
+		\App\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Slideshow', 'link' => 'slideshow');
+
+		\App\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('slideshow/icon.svg'), 'link'=>'slideshow', 'title' => __('Slideshow'), 'description' => __('Slideshow images'));
+		parent::run();
+	}
+}
+return new Bundle;
