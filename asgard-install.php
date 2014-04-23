0cc43c707d01f7f33dc8537c8ebf27aee32878b5
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:22:17 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -6,5 +6,5 @@ require _CORE_DIR_.'core.php';
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/slideshow', _DIR_.'app/slideshow');
 \Asgard\Utils\FileManager::copy(__DIR__.'/web/slideshow', _DIR_.'web/slideshow');
 
-\Asgard\Orm\MigrationsManager::addMigrationFile(__DIR__.'/migrations/Slideshow.php');
-\Asgard\Orm\MigrationsManager::migrate('Slideshow');
+\Asgard\Orm\Libs\MigrationsManager::addMigrationFile(__DIR__.'/migrations/Slideshow.php');
+\Asgard\Orm\Libs\MigrationsManager::migrate('Slideshow');
