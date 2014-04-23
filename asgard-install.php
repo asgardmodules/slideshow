5993601cf0ad39f96f7a584eb5dc4ea0f43e83b2
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:31:28 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'paths.php';
-require _CORE_DIR_.'core.php';
+require_once _VENDOR_DIR_.'autoload.php'; #composer autoloader
 \Asgard\Core\App::loadDefaultApp();
 
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/slideshow', _DIR_.'app/slideshow');
